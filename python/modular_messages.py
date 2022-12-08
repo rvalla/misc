59c3da79b33b25af9c1cdb34ebf74e3405706921
Rodrigo Valla <rodrigovalla@protonmail.ch>
2022-12-08 20:34:48 -0300

    Few minor changes...

--- python/modular_messages.py
+++ python/modular_messages.py
@@ -1,14 +1,17 @@
+import time as tm
 import random as rd
 import json as js
 
+print("I am ready to do my magic...", end="\n")
+start_time = tm.time()
 input = open("data/modules_input_ex.csv").readlines()[1:]
 input_links = js.load(open("data/modules_links_ex.json"))
 output = open("data/modules_output_ex.csv", "w")
 links = [] #Here we store de possible message fragments (a 3d matrix)...
 links_count = 4
 levels = 3
-conectors = [", ", ". ", " aunque ", ".\n"] #The size of this lists is links_count!
-capital = [False, False, True, False]
+conectors = [" y ", ". ", ". ", ".\n"] #The size of this lists is links_count!
+capital = [False, False, True, True]
 
 for i in range(links_count): #Let's build the matrix...
 	l = []
@@ -33,3 +36,14 @@ for i in input:
 	output.write(message)
 
 output.close()
+
+total_time = tm.time() - start_time
+time = ""
+minutes = total_time // 60
+seconds = total_time - minutes * 60
+seconds = round(seconds, 4)
+time = "{:02d}".format(int(minutes))
+time += ":"
+time += "{:07.4f}".format(seconds) 
+print("I have created " + str(len(input)) + " messages.", end="\n")
+print("This took me only " + str(time) + "!", end="\n")
