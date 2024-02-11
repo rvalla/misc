130f7f7f36f11e89a737bc9d336fa59c75453a9e
Rodrigo Valla <rodrigovalla@protonmail.ch>
2024-02-11 20:16:33 -0300

    Classifying words...

--- python/word_classification.py
+++ python/word_classification.py
@@ -1,24 +1,37 @@
-files = ["borges_funes.txt", "borges_biblioteca.txt", "borges_jardin.txt"]
+files = ["borges_biblioteca.txt", "borges_funes.txt", "borges_jardin.txt", "borges_ruinas.txt",
+        "cortazar_casa.txt", "cortazar_escalera.txt", "cortazar_reloj.txt"]
 
 word_sets = [set() for i in range(15)]
 
-for i in range(3):
+for i in range(len(files)):
     lines = open("data/" + files[i]).readlines()
     for l in lines:
         cleaned = l.replace("\n", "")
         for word in cleaned.split(" "):
             position = len(word) - 2
             if position >= 0 and position < 15:
-                word_sets[position].add(word)
+                word_sets[position].add(word.lower())
     
     output = open("output/words_" + files[i], "w")
     for s in word_sets:
         line = ""
         for word in s:
-            line += word.lower()
-            line += ","
+            line += word
+            line += " "
         line += "\n"
+        line = line.replace(".", "")
+        line = line.replace(",", "")
+        line = line.replace(";", "")
+        line = line.replace(":", "")
+        line = line.replace("-", "")
+        line = line.replace("(", "")
+        line = line.replace(")", "")
+        line = line.replace("¿", "")
+        line = line.replace("?", "")
+        line = line.replace("!", "")
+        line = line.replace("¡", "")
         output.write(line)
+    output.close()
     
     for s in word_sets:
         s.clear()
