ba44c3441d5643d0fb608a9a7ff7723bd56c1b34
Rodrigo Valla <rodrigovalla@protonmail.ch>
2025-04-05 10:49:34 -0300

    Studying the BCRA API...

--- python/bcra.py
+++ python/bcra.py
@@ -1,5 +1,9 @@
 import requests
 
-data = requests.get("https://api.bcra.gob.ar/estadisticas/v2.0/datosvariable/31/2024-08-31/2024-08-31", verify=False).json()
-print(data["results"])
+data = requests.get("https://api.bcra.gob.ar/estadisticas/v3.0/Monetarias", verify=False).json()
+print(data)
 
+
+  
+  
+   
