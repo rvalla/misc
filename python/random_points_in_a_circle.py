11d033bcc71435c4ff2ec3f9136a1e8acc3e4bde
Rodrigo Valla <rodrigovalla@protonmail.ch>
2025-08-16 15:22:40 -0300

    Improving random points in a circle...

--- python/random_points_in_a_circle.py
+++ python/random_points_in_a_circle.py
@@ -3,7 +3,7 @@ import time as tm
 import math
 import matplotlib.pyplot as plt
 
-cycles = 12000
+cycles = [50,100,500,1000,5000,10000]
 start_time = None
 print("--- RANDOM POINTS IN A CIRCLE: Testing alternatives", end="\n")
 print("Number of points: " + str(cycles), end="\n")
@@ -14,22 +14,24 @@ def get_polar_point(radious):
   return (a, h), (math.cos(a) * h, math.sin(a) * h)
 
 def get_pitagoras_point(radious):
-  x = radious - rd.random() * radious * 2
+  x = min(rd.random(), rd.random()) * radious
+  if rd.random() < 0.5:
+    x = x*-1
   y_limit = math.sqrt(radious*radious - x*x)
   y = rd.random() * y_limit * 2 - y_limit
   return (x, y)
 
 def get_pitagoras_point_rq(radious):
-  x = rd.random() * radious
+  x = min(rd.random(), rd.random()) * radious
   y = rd.random() * (math.sqrt(radious*radious - x*x))
   return (rd.choice([-1,1])*x, rd.choice([-1,1])*y)
 
 def get_pitagoras_point_crq(radious):
   if rd.random() < 0.5:
-    x = rd.random() * radious
+    x = min(rd.random(), rd.random()) * radious
     y = rd.random() * (math.sqrt(radious*radious - x*x))
   else:
-    y = rd.random() * radious
+    y = min(rd.random(), rd.random()) * radious
     x = rd.random() * (math.sqrt(radious*radious - y*y))
   return (rd.choice([-1,1])*x, rd.choice([-1,1])*y)
 
@@ -43,7 +45,8 @@ def visualize(x, y, ox, oy, title):
   plt.subplot2grid((2, 3), (1, 2), 1, 1)
   plt.hist(oy, 10)
   plt.tight_layout()
-  plt.savefig("output/random_points_in_a_circle_" + str(cycles) + "_" + title + ".png")
+  plt.savefig("output/random_points_in_a_circle_" + title + ".png")
+  plt.close(f)
   print("Data visualization saved!", end="\n")
 
 def format_time(time):
@@ -61,52 +64,54 @@ def get_working_time(start, end):
 	formatedTime = format_time(time)
 	return formatedTime
 
-polar_angles = []
-polar_magnitudes = []
-polar_x = []
-polar_y = []
-pitagoras_x = []
-pitagoras_y = []
-pitagorasrq_x = []
-pitagorasrq_y = []
-pitagorascrq_x = []
-pitagorascrq_y = []
-
 print("Generating points using polar coordinates", end="\n")
 start_time = tm.time()
-for i in range(cycles):
-  am, p = get_polar_point(25)
-  polar_angles.append(am[0])
-  polar_magnitudes.append(am[1])
-  polar_x.append(p[0])
-  polar_y.append(p[1])
+for c in cycles:
+  polar_angles = []
+  polar_magnitudes = []
+  polar_x = []
+  polar_y = []
+  for i in range(c):
+    am, p = get_polar_point(25)
+    polar_angles.append(am[0])
+    polar_magnitudes.append(am[1])
+    polar_x.append(p[0])
+    polar_y.append(p[1])
+  visualize(polar_x, polar_y, polar_angles, polar_magnitudes, str(c) + "p polar generated points")
 print("This took me " + get_working_time(start_time, tm.time()), end="\n")
-visualize(polar_x, polar_y, polar_angles, polar_magnitudes, "polar generated points")
 
 print("Generating points with pitagoras equation", end="\n")
 start_time = tm.time()
-for i in range(cycles):
-  p = get_pitagoras_point(25)
-  pitagoras_x.append(p[0])
-  pitagoras_y.append(p[1])
+for c in cycles:
+  pitagoras_x = []
+  pitagoras_y = []
+  for i in range(c):
+    p = get_pitagoras_point(25)
+    pitagoras_x.append(p[0])
+    pitagoras_y.append(p[1])
+  visualize(pitagoras_x, pitagoras_y, pitagoras_x, pitagoras_y, str(c) + "p pitagoras")
 print("This took me " + get_working_time(start_time, tm.time()), end="\n")
-visualize(pitagoras_x, pitagoras_y, pitagoras_x, pitagoras_y, "pitagoras")
 
 print("Generating points with pitagoras equation selecting a random quadrant", end="\n")
 start_time = tm.time()
-for i in range(cycles):
-  p = get_pitagoras_point_rq(25)
-  pitagorasrq_x.append(p[0])
-  pitagorasrq_y.append(p[1])
+for c in cycles:
+  pitagoras_x = []
+  pitagoras_y = []
+  for i in range(c):
+    p = get_pitagoras_point_rq(25)
+    pitagoras_x.append(p[0])
+    pitagoras_y.append(p[1])
+  visualize(pitagoras_x, pitagoras_y, pitagoras_x, pitagoras_y, str(c) + "p pitagoras random quadrant")
 print("This took me " + get_working_time(start_time, tm.time()), end="\n")
-visualize(pitagorasrq_x, pitagorasrq_y, pitagorasrq_x, pitagorasrq_y, "pitagoras random quadrant")
 
 print("Generating points with pitagoras equation selecting a random quadrant", end="\n")
 start_time = tm.time()
-for i in range(cycles):
-  p = get_pitagoras_point_crq(25)
-  pitagorascrq_x.append(p[0])
-  pitagorascrq_y.append(p[1])
+for c in cycles:
+  pitagoras_x = []
+  pitagoras_y = []
+  for i in range(c):
+    p = get_pitagoras_point_crq(25)
+    pitagoras_x.append(p[0])
+    pitagoras_y.append(p[1])
+  visualize(pitagoras_x, pitagoras_y, pitagoras_x, pitagoras_y, str(c) + "p pitagoras corrected random quadrant")
 print("This took me " + get_working_time(start_time, tm.time()), end="\n")
-visualize(pitagorascrq_x, pitagorascrq_y, pitagorascrq_x, pitagorascrq_y, "pitagoras corrected random quadrant")
-
